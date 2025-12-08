c399e9b4274d8f8f7942948b4662a037bc178415
RoyPrinsGH <royprinsgm@gmail.com>
2025-12-08 22:05:30 +0100

    Split off lexer into own file

---
+++ src/lexer.rs
@@ -0,0 +1,270 @@
+use std::{
+    iter::{self},
+    ops::Range,
+};
+
+#[derive(Debug)]
+pub enum Token<'s> {
+    LeftParenthesis,
+    RightParenthesis,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Asterisk,
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    Identifier(&'s str),
+    String(&'s str),
+    Number(f64),
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+    EOF,
+}
+
+#[derive(Debug)]
+pub enum LexerError {
+    UnrecognisedToken,
+    UnfinishedString,
+    InvalidNumber,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum SpanKind {
+    Unrecognised,
+    String,
+    Number,
+    Identifier,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Span {
+    from: usize,
+    kind: SpanKind,
+}
+
+#[derive(Debug)]
+struct SpanTracker {
+    span: Option<Span>,
+}
+
+impl SpanTracker {
+    fn new() -> Self {
+        Self { span: None }
+    }
+
+    fn track_new(&mut self, ix: usize, kind: SpanKind) {
+        self.span = Some(Span { from: ix, kind })
+    }
+
+    fn reset(&mut self) {
+        self.span = None;
+    }
+}
+
+fn ident_to_token<'s>(ident: &'s str) -> Token<'s> {
+    match ident {
+        "and" => Token::And,
+        "class" => Token::Class,
+        "else" => Token::Else,
+        "false" => Token::False,
+        "for" => Token::For,
+        "fun" => Token::Fun,
+        "if" => Token::If,
+        "nil" => Token::Nil,
+        "or" => Token::Or,
+        "print" => Token::Print,
+        "return" => Token::Return,
+        "super" => Token::Super,
+        "this" => Token::This,
+        "true" => Token::True,
+        "var" => Token::Var,
+        "while" => Token::While,
+        non_reserved => Token::Identifier(non_reserved),
+    }
+}
+
+pub fn lex<'a>(source: &'a str) -> impl Iterator<Item = Result<Token<'a>, (Range<usize>, LexerError)>> {
+    let mut span_tracker = SpanTracker::new();
+    let mut chars = source.char_indices().fuse().peekable();
+    iter::from_fn(move || {
+        let token_or_err = loop {
+            match (span_tracker.span, chars.next(), chars.peek()) {
+                (None, Some((ix, c)), maybe_nc) => {
+                    let token = match c {
+                        '(' => Token::LeftParenthesis,
+                        ')' => Token::RightParenthesis,
+                        '{' => Token::LeftBrace,
+                        '}' => Token::RightBrace,
+                        ',' => Token::Comma,
+                        '.' => Token::Dot,
+                        '-' => Token::Minus,
+                        '+' => Token::Plus,
+                        ';' => Token::Semicolon,
+                        '*' => Token::Asterisk,
+                        '!' => match maybe_nc {
+                            Some((_, '=')) => {
+                                _ = chars.next();
+                                Token::BangEqual
+                            }
+                            _ => Token::Bang,
+                        },
+                        '=' => match maybe_nc {
+                            Some((_, '=')) => {
+                                _ = chars.next();
+                                Token::EqualEqual
+                            }
+                            _ => Token::Equal,
+                        },
+                        '<' => match maybe_nc {
+                            Some((_, '=')) => {
+                                _ = chars.next();
+                                Token::LessEqual
+                            }
+                            _ => Token::Less,
+                        },
+                        '>' => match maybe_nc {
+                            Some((_, '=')) => {
+                                _ = chars.next();
+                                Token::GreaterEqual
+                            }
+                            _ => Token::Greater,
+                        },
+                        '/' => match maybe_nc {
+                            Some((_, '/')) => {
+                                _ = chars.next();
+                                while let Some((_, nc)) = chars.next() {
+                                    match nc {
+                                        '\n' => {
+                                            break;
+                                        }
+                                        _ => {
+                                            continue;
+                                        }
+                                    }
+                                }
+                                continue;
+                            }
+                            _ => Token::Slash,
+                        },
+                        '\n' | ' ' => continue,
+                        '\"' => {
+                            span_tracker.track_new(ix, SpanKind::String);
+                            continue;
+                        }
+                        c if c.is_ascii_digit() => {
+                            if let Some((_, nc)) = maybe_nc
+                                && (nc.is_ascii_digit() || *nc == '.')
+                            {
+                                span_tracker.track_new(ix, SpanKind::Number);
+                                continue;
+                            } else {
+                                Token::Number((c as u8 - '0' as u8) as f64)
+                            }
+                        }
+                        c if c.is_alphabetic() => {
+                            if let Some((_, nc)) = maybe_nc
+                                && nc.is_alphanumeric()
+                            {
+                                span_tracker.track_new(ix, SpanKind::Identifier);
+                                continue;
+                            } else {
+                                ident_to_token(&source[ix..=ix])
+                            }
+                        }
+                        _ => {
+                            span_tracker.track_new(ix, SpanKind::Unrecognised);
+                            continue;
+                        }
+                    };
+
+                    break Ok(token);
+                }
+                (None, None, _) => return None,
+                (Some(Span { from, kind: SpanKind::String }), Some((ix, c)), _) => match c {
+                    '\"' => {
+                        span_tracker.reset();
+                        break Ok(Token::String(&source[from + 1..ix]));
+                    }
+                    '\\' => {
+                        // Escape char: Advance without looking
+                        _ = chars.next();
+                        continue;
+                    }
+                    _ => continue,
+                },
+                (Some(Span { from, kind: SpanKind::String }), None, _) => {
+                    span_tracker.reset();
+                    break Err((from..source.len(), LexerError::UnfinishedString));
+                }
+                (Some(Span { from, kind: SpanKind::Number }), _, Some((nc_ix, nc))) => {
+                    if nc.is_ascii_digit() || *nc == '.' {
+                        continue;
+                    } else {
+                        span_tracker.reset();
+                        break (&source[from..*nc_ix])
+                            .parse()
+                            .map(|float| Token::Number(float))
+                            .map_err(|_| (from..*nc_ix, LexerError::InvalidNumber));
+                    }
+                }
+                (Some(Span { from, kind: SpanKind::Number }), _, None) => {
+                    break (&source[from..])
+                        .parse()
+                        .map(|float| Token::Number(float))
+                        .map_err(|_| ((from..source.len()), LexerError::InvalidNumber));
+                }
+                (Some(Span { from, kind: SpanKind::Unrecognised }), Some((ix, c)), _) => match c {
+                    ' ' => {
+                        span_tracker.reset();
+                        break Err(((from..ix), LexerError::UnrecognisedToken));
+                    }
+                    _ => continue,
+                },
+                (Some(Span { from, kind: SpanKind::Unrecognised }), None, _) => {
+                    span_tracker.reset();
+                    break Err(((from..source.len()), LexerError::UnrecognisedToken));
+                }
+                (Some(Span { from, kind: SpanKind::Identifier }), _, Some((nc_ix, nc))) => {
+                    if nc.is_alphanumeric() {
+                        continue;
+                    } else {
+                        span_tracker.reset();
+                        break Ok(ident_to_token(&source[from..*nc_ix]));
+                    }
+                }
+                (Some(Span { from, kind: SpanKind::Identifier }), _, None) => {
+                    span_tracker.reset();
+                    break Ok(ident_to_token(&source[from..]));
+                }
+            }
+        };
+
+        Some(token_or_err)
+    })
+    .chain(iter::once(Ok(Token::EOF)))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 use std::{
     fs::read_to_string,
     io::{Write, stdin, stdout},
-    iter::{self},
+    ops::Range,
     path::{Path, PathBuf},
 };
 
-use ariadne::{Cache, Color, Label, Report, ReportKind, Source, Span};
+use ariadne::{Color, Label, Report, ReportKind, Source};
 use clap::Parser;
 use miette::{Context, IntoDiagnostic, Result, miette};
 
+use crate::lexer::{LexerError, lex};
+
+pub mod lexer;
+
 #[derive(Parser, Debug)]
 struct Args {
-    /// Name of the person to greet
+    /// Name of the JLox script to run
     script: Option<PathBuf>,
 }
 
@@ -26,11 +30,7 @@ fn main() -> Result<()> {
 
 fn run_script(script_path: impl AsRef<Path>) -> Result<()> {
     let script_path = script_path.as_ref();
-
-    let script_content = read_to_string(script_path)
-        .into_diagnostic()
-        .wrap_err(miette!("Specified script {:?} not found", script_path))?;
-
+    let script_content = read_to_string(script_path).into_diagnostic().wrap_err(miette!("Specified script {:?} not found", script_path))?;
     run(script_content)
 }
 
@@ -40,10 +40,7 @@ fn run_prompt() -> Result<()> {
     loop {
         print!("> ");
 
-        stdout()
-            .flush()
-            .into_diagnostic()
-            .wrap_err(miette!("Failed to flush stdout: {:?}", line_to_execute))?;
+        stdout().flush().into_diagnostic().wrap_err(miette!("Failed to flush stdout: {:?}", line_to_execute))?;
 
         stdin()
             .read_line(&mut line_to_execute)
@@ -56,248 +53,30 @@ fn run_prompt() -> Result<()> {
     }
 }
 
-#[derive(Debug)]
-enum Token<'s> {
-    // Single-character tokens.
-    LeftParenthesis,
-    RightParenthesis,
-    LeftBrace,
-    RightBrace,
-    Comma,
-    Dot,
-    Minus,
-    Plus,
-    Semicolon,
-    Slash,
-    Asterisk,
-    // One or two character tokens.
-    Bang,
-    BangEqual,
-    Equal,
-    EqualEqual,
-    Greater,
-    GreaterEqual,
-    Less,
-    LessEqual,
-    // Literals.
-    Identifier,
-    String(&'s str),
-    Number(f64),
-    // Keywords.
-    And,
-    Class,
-    Else,
-    False,
-    Fun,
-    For,
-    If,
-    Nil,
-    Or,
-    Print,
-    Return,
-    Super,
-    This,
-    True,
-    Var,
-    While,
-    EOF,
-}
-
-#[derive(Debug)]
-enum LexerError {
-    UnrecognisedToken,
-    UnfinishedString,
-    InvalidNumber,
-}
-
-#[derive(Debug)]
-struct TokenSpan {
-    start: usize,
-    end: usize,
-}
-
-#[derive(Clone, Copy, Debug)]
-enum LexerScope {
-    None,
-    Unrecognised,
-    String,
-    Number,
-}
-
-fn lex<'a>(script: &'a str) -> impl Iterator<Item = Result<Token<'a>, (TokenSpan, LexerError)>> {
-    let mut scope = (0, LexerScope::None);
-    let mut chars = script.char_indices().fuse().peekable();
-    let mut to_yield = Vec::new();
-    let mut exit = false;
-
-    iter::from_fn(move || {
-        if let Some(item) = to_yield.pop() {
-            return Some(item);
-        }
-
-        if exit {
-            return None;
-        }
-
-        loop {
-            match (scope, chars.next(), chars.peek()) {
-                ((_, LexerScope::None), Some((ix, c)), maybe_nc) => {
-                    let token = match c {
-                        '(' => Token::LeftParenthesis,
-                        ')' => Token::RightParenthesis,
-                        '{' => Token::LeftBrace,
-                        '}' => Token::RightBrace,
-                        ',' => Token::Comma,
-                        '.' => Token::Dot,
-                        '-' => Token::Minus,
-                        '+' => Token::Plus,
-                        ';' => Token::Semicolon,
-                        '*' => Token::Asterisk,
-                        '\n' | ' ' => {
-                            continue;
-                        }
-                        '\"' => {
-                            scope = (ix, LexerScope::String);
-                            continue;
-                        }
-                        c if c.is_ascii_digit() => {
-                            if let Some((_, nc)) = maybe_nc
-                                && (nc.is_ascii_digit() || *nc == '.')
-                            {
-                                scope = (ix, LexerScope::Number);
-                                continue;
-                            } else {
-                                Token::Number((c as u8 - '0' as u8) as f64)
-                            }
-                        }
-                        _ => {
-                            scope = (ix, LexerScope::Unrecognised);
-                            continue;
-                        }
-                    };
-
-                    to_yield.push(Ok(token));
-                }
-                ((_, LexerScope::None), None, _) => {
-                    exit = true;
-                }
-                ((start, LexerScope::String), Some((ix, c)), _) => match c {
-                    '\"' => {
-                        to_yield.push(Ok(Token::String(&script[start + 1..ix])));
-                        scope = (ix, LexerScope::None)
-                    }
-                    '\\' => {
-                        // Escape char: Advance without looking
-                        _ = chars.next();
-                        continue;
-                    }
-                    _ => {
-                        continue;
-                    }
-                },
-                ((start, LexerScope::String), None, _) => {
-                    to_yield.push(Err((
-                        TokenSpan {
-                            start,
-                            end: script.len(),
-                        },
-                        LexerError::UnfinishedString,
-                    )));
-                    exit = true;
-                }
-                ((start, LexerScope::Number), _, Some((nc_ix, nc))) => {
-                    if nc.is_ascii_digit() || *nc == '.' {
-                        continue;
-                    } else {
-                        let result = (&script[start..*nc_ix])
-                            .parse()
-                            .map(|float| Token::Number(float))
-                            .map_err(|_| {
-                                (TokenSpan { start, end: *nc_ix }, LexerError::InvalidNumber)
-                            });
-
-                        to_yield.push(result);
-                        scope = (*nc_ix, LexerScope::None);
-                    }
-                }
-                ((start, LexerScope::Number), _, None) => {
-                    let result = (&script[start..])
-                        .parse()
-                        .map(|float| Token::Number(float))
-                        .map_err(|_| {
-                            (
-                                TokenSpan {
-                                    start,
-                                    end: script.len(),
-                                },
-                                LexerError::InvalidNumber,
-                            )
-                        });
-
-                    to_yield.push(result);
-                    exit = true;
-                }
-                ((start, LexerScope::Unrecognised), Some((ix, c)), _) => match c {
-                    ' ' => {
-                        to_yield.push(Err((
-                            TokenSpan { start, end: ix },
-                            LexerError::UnrecognisedToken,
-                        )));
-                        scope = (ix, LexerScope::None);
-                    }
-                    _ => continue,
-                },
-                ((start, LexerScope::Unrecognised), None, _) => {
-                    to_yield.push(Err((
-                        TokenSpan {
-                            start,
-                            end: script.len(),
-                        },
-                        LexerError::UnrecognisedToken,
-                    )));
-                    exit = true;
-                }
-            }
-            break;
-        }
-
-        to_yield.pop()
-    })
-}
-
-fn report_lexer_error(script_name: &str, script_source: &str, span: TokenSpan, error: LexerError) {
+fn report_lexer_error(script_name: &str, script_source: &str, span: Range<usize>, error: LexerError) {
     let mut report_builder = Report::build(ReportKind::Error, (script_name, span.start..span.end));
 
     match error {
         LexerError::UnfinishedString => {
-            report_builder = report_builder
-                .with_code(1)
-                .with_message("Unfinished string")
-                .with_label(
-                    Label::new((script_name, span.end..span.end))
-                        .with_message("Expected end of string here")
-                        .with_color(Color::Red),
-                )
+            report_builder = report_builder.with_code(1).with_message("Unfinished string").with_label(
+                Label::new((script_name, span.end..span.end))
+                    .with_message("Expected end of string here: Is your source well-formatted?")
+                    .with_color(Color::Red),
+            )
         }
         LexerError::UnrecognisedToken => {
-            report_builder = report_builder
-                .with_code(2)
-                .with_message("Unrecognised token")
-                .with_label(
-                    Label::new((script_name, span.start..span.end))
-                        .with_message("What does this mean?")
-                        .with_color(Color::Red),
-                )
+            report_builder = report_builder.with_code(2).with_message("Unrecognised token").with_label(
+                Label::new((script_name, span.start..span.end))
+                    .with_message("This is not a valid JLox token")
+                    .with_color(Color::Red),
+            )
         }
         LexerError::InvalidNumber => {
-            report_builder = report_builder
-                .with_code(3)
-                .with_message("Invalid number")
-                .with_label(
-                    Label::new((script_name, span.start..span.end))
-                        .with_message("This does not parse to a f64")
-                        .with_color(Color::Red),
-                )
+            report_builder = report_builder.with_code(3).with_message("Invalid number").with_label(
+                Label::new((script_name, span.start..span.end))
+                    .with_message("This does not parse to a f64")
+                    .with_color(Color::Red),
+            )
         }
     }
 
